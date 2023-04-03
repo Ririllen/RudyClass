ec548c12a1639a76a9ab935be69785f3079eef6d
Elena Teaca <ririteaca@gmail.com>
2023-04-03 20:01:58 +0300

    Variant2 - ES6

--- variants_Class.js
+++ variants_Class.js
@@ -1,5 +1,36 @@
+//Variant 2
+class RudyES6 {
+    constructor(nume, tipRasa, colorPar, birthDay) {
+        this.nume = nume;
+        this.tipRasa = tipRasa;
+        this.colorPar = colorPar;
+        this.birthDay = birthDay;
+        this.vaccin = [];
+        this.passport = false;
+    }
+    descriptionRudy() {
+        return `${this.nume} este un caine de rasa ${this.tipRasa}, are culoarea  ${this.colorPar}, s-a nascut in anul ${this.birthDay} si ${!this.passport ? "nu " : ""} are voie sa iasa din Moldova si are urmatoarele vaccine ${this.vaccin[0].nume} `;
+     }
+     
+    adaugareVaccin(vaccin) {
+         this.vaccin.push(vaccin);
+     }
+    
+    afiseazaIstoriculVaccinarii(){
+        for(let i=0; i < this.vaccin.length; i++){
+           console.log(" vaccin  "+ this.vaccin[i].nume + " data de "+ this.vaccin[i].data )
+        }
+    }
+}
+
+const rudyThree = new RudyES6('Laika','labrador', 'gold','2020');
+rudyThree.adaugareVaccin({nume: "hepatita canina", data:"2020-03-17" } );
+rudyThree.afiseazaIstoriculVaccinarii();
+console.log(rudyThree.descriptionRudy());
+
 //Varianta 1 - Functii constructor
-const Rudy = function (tipRasa, colorPar, birthDay) {
+const Rudy = function (nume,tipRasa, colorPar, birthDay) {
+    this.nume = nume;
     this.tipRasa = tipRasa;
     this.colorPar = colorPar;
     this.birthDay = birthDay;
@@ -8,7 +39,7 @@ const Rudy = function (tipRasa, colorPar, birthDay) {
 }
 
 Rudy.prototype.descriptionRudy = function() {
-    return `Rudy este un caine de rasa ${this.tipRasa}, are culoarea  ${this.colorPar}, s-a nascut in anul ${this.birthDay} si ${!this.passport ? "nu " : ""} are voie sa iasa din Moldova si are urmatoarele vaccine ${this.vaccin[0].nume} `;
+    return `${this.nume} este un caine de rasa ${this.tipRasa}, are culoarea  ${this.colorPar}, s-a nascut in anul ${this.birthDay} si ${!this.passport ? "nu " : ""} are voie sa iasa din Moldova si are urmatoarele vaccine ${this.vaccin[0].nume} `;
  }
  
  Rudy.prototype.adaugareVaccin = function(vaccin) {
@@ -21,7 +52,12 @@ Rudy.prototype.descriptionRudy = function() {
     }
 }
 
-const rudyOne = new Rudy('labrador', 'gold','2020');
+const rudyOne = new Rudy('Belka','labrador', 'gold','2020');
 rudyOne.adaugareVaccin({nume: "hepatita canina", data:"2020-03-17" } );
 rudyOne.afiseazaIstoriculVaccinarii();
 console.log(rudyOne.descriptionRudy());
+
+const rudyTwo = new Rudy('Strelka', 'husky', 'tarcat','2021');
+rudyTwo.adaugareVaccin({nume: "hepatita labradina", data:"2021-04-03" } );
+rudyTwo.afiseazaIstoriculVaccinarii();
+console.log(rudyTwo.descriptionRudy());
